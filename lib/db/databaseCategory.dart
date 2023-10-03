f1644c95f39b36d098ef021e3d47f978886632db
JavierDuranFlores <duranfloresj7@gmail.com>
2023-10-03 15:04:13 -0600

    agregande databaseCategory.dart

--- lib/db/databaseCategory.dart
+++ lib/db/databaseCategory.dart
@@ -1,19 +1,38 @@
-
 import 'package:app_2/db/notesdb.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
+import 'dart:math';
+import 'dart:typed_data';
 
 class DatabaseHelper {
-  //instancia unica
+  // Instancia única de la clase para el patrón Singleton.
   static final DatabaseHelper _instance = DatabaseHelper.internal();
-  // Constructor factory para crear una única instancia de DatabaseHelper
+
+  // Fábrica para obtener la instancia única.
   factory DatabaseHelper() => _instance;
+
+  // Variable para almacenar la base de datos.
   static Database? _db;
 
-  // Método para imprimir los nombres de las tablas en la base de datos
+  // Clave y cifrador para la encriptación de datos.
+  late final encrypt.Key key;
+  late final encrypt.Encrypter encrypter;
+
+  // Constructor privado para la inicialización.
+  DatabaseHelper.internal() {
+    // Clave de cifrado generada aleatoriamente al inicio.
+    final random = Random.secure();
+    key = encrypt.Key(Uint8List.fromList(
+        List<int>.generate(32, (index) => random.nextInt(256))));
+    encrypter = encrypt.Encrypter(encrypt.AES(key, mode: encrypt.AESMode.cbc));
+  }
+
+  // Función para imprimir nombres de tablas en la base de datos.
   Future<void> printTableNames() async {
     final db = await this.db;
-    final result = await db!.rawQuery("SELECT name FROM sqlite_master WHERE type='table'");
+    final result =
+        await db!.rawQuery("SELECT name FROM sqlite_master WHERE type='table'");
     final tableNames = result.map((row) => row['name'] as String).toList();
 
     for (final tableName in tableNames) {
@@ -21,14 +40,14 @@ class DatabaseHelper {
     }
   }
 
-
+  // Función para obtener la ruta de la base de datos.
   Future<String> getDatabaseName() async {
     final databasesPath = await getDatabasesPath();
     final path = join(databasesPath, 'gestorypassword.db');
     return path;
   }
 
-  // Método asincrónico para obtener la base de datos SQLite
+  // Getter para obtener la base de datos.
   Future<Database?> get db async {
     if (_db != null) {
       return _db;
@@ -36,19 +55,17 @@ class DatabaseHelper {
     _db = await initDb();
     return _db;
   }
-  // Constructor privado para garantizar que solo se pueda crear una instancia
-  DatabaseHelper.internal();
-  // Método para inicializar la base de datos
+
+  // Función para inicializar la base de datos si no existe.
   Future<Database> initDb() async {
     final databasesPath = await getDatabasesPath();
     final path = join(databasesPath, 'categories.db');
     final databaseName = await getDatabaseName();
     print('Nombre de la base de datos: $databaseName');
-    // Abre la base de datos o crea una nueva si no existe
-    //return await openDatabase(path, version: 1, onCreate: _onCreate);
     return await openDatabase(path, version: 3, onCreate: _onCreate);
   }
-  // Método para crear la tabla 'categories, images' en la base de datos
+
+  // Función para crear las tablas en la base de datos.
   void _onCreate(Database db, int newVersion) async {
     await db.execute('''
       CREATE TABLE categories (
@@ -63,8 +80,9 @@ class DatabaseHelper {
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         title TEXT,
         content TEXT,
-        category_id INTEGER,  -- Agrega una columna para la clave foránea
-        FOREIGN KEY (category_id) REFERENCES categories (id)  -- Define la clave foránea
+        category_id INTEGER,
+        iv TEXT,  // Campo para almacenar el IV
+        FOREIGN KEY (category_id) REFERENCES categories (id)
       )
     ''');
 
@@ -76,20 +94,16 @@ class DatabaseHelper {
     ''');
   }
 
-  //Inserte la nueva imagen y si ya hay una imegen en la base de datos la elimina
+  // Función para insertar una ruta de imagen en la base de datos.
   Future<void> insertImage(String imagePath) async {
     final db = await this.db;
-    // Antes de insertar la nueva imagen, elimina la imagen anterior si existe
     await db!.delete('images');
-    // Inserta la nueva imagen en la tabla
     await db.insert('images', {'image_path': imagePath});
-
   }
 
+  // Función para insertar una categoría en la base de datos.
   Future<int> insertCategory(Category category) async {
     final dbClient = await db;
-
-    // Verificar si ya existe una categoría con el mismo color
     final existingCategories = await dbClient!.query(
       'categories',
       where: 'color = ?',
@@ -97,111 +111,83 @@ class DatabaseHelper {
     );
 
     if (existingCategories.isNotEmpty) {
-      // Ya existe una categoría con el mismo color, puedes manejar el error aquí
-      return -1; // O algún otro valor que indique que la inserción falló
+      return -1;
     }
 
-    // Inserta la categoría en la tabla 'categories' sin especificar el ID
     return await dbClient.insert('categories', category.toMap());
   }
 
-  /*
-  // Método para insertar una categoría en la base de datos
-  Future<int> insertCategory(Category category) async {
-    final dbClient = await db;
-
-    // Verificar si ya existe una categoría con el mismo color
-    final existingCategories = await dbClient!.query(
-      'categories',
-      where: 'color = ?',
-      whereArgs: [category.color],
-    );
-
-    if (existingCategories.isNotEmpty) {
-      // Ya existe una categoría con el mismo color, puedes manejar el error aquí
-      return -1; // O algún otro valor que indique que la inserción falló
-    }
-    // Inserta la categoría en la tabla 'categories'
-    return await dbClient.insert('categories', category.toMap());
-  }*/
-
-  // Método para obtener todas las categorías de la base de datos
+  // Función para obtener todas las categorías de la base de datos.
   Future<List<Category>> getCategories() async {
     final dbClient = await db;
     final list = await dbClient!.query('categories');
     return list.map((json) => Category.fromMap(json)).toList();
   }
-  // Obtiene todas las notas de la base de datos y las convierte en una lista de `Notea`
+
+  // Función para obtener todas las notas de la base de datos.
   Future<List<Notea>> getAllNotes() async {
     final db = await _instance.db;
     final result = await db!.query('notes');
-    return result.map((json) => Notea.fromMap(json)).toList();
+    final notes = result.map((json) {
+      final content = json['content'] as String?;
+      final iv = json['iv'] as String?;
+      if (content != null && iv != null) {
+        final decryptedText = decryptText(content, iv);
+        json['content'] = decryptedText;
+      }
+      return Notea.fromMap(json);
+    }).toList();
+    return notes;
   }
 
-  /*Future<int> deleteCategory(String categoryName) async {
-    final dbClient = await db;
-    return await dbClient!.delete(
-      'categories',
-      where: 'name = ?',
-      whereArgs: [categoryName],
-    );
-  }*/
+  // Función para cifrar un texto con un IV y devolver el texto cifrado en Base64.
+  String encryptText(String text, encrypt.IV iv) {
+    final encryptedText = encrypter.encrypt(text, iv: iv);
+    return encryptedText.base64;
+  }
 
-// Funciones de la bd notes
-// Inserta una nueva nota en la base de datos
+  // Función para descifrar un texto cifrado en Base64 utilizando un IV.
+  String decryptText(String encryptedText, String ivString) {
+    final iv = encrypt.IV.fromBase64(ivString);
+    final encrypted = encrypt.Encrypted.fromBase64(encryptedText);
+    final decryptedText = encrypter.decrypt(encrypted, iv: iv);
+    return decryptedText;
+  }
+
+  // Función para insertar una nota en la base de datos.
   Future<int?> insert(Notea note, int categoryId) async {
     final db = await _instance.db;
-
     final noteMap = note.toMap();
+    final textToEncrypt = note.content;
+    final random = Random.secure();
+    final iv = encrypt.IV(Uint8List.fromList(
+        List<int>.generate(16, (index) => random.nextInt(256))));
     noteMap['category_id'] = categoryId;
+    noteMap['content'] = encryptText(textToEncrypt, iv);
+    noteMap['iv'] = iv.base64; // Guarda el IV en la base de datos
 
-    return await db?.insert('notes', note.toMap());
+    final insertedId = await db?.insert('notes', noteMap);
+
+    // Verifica si la encriptación se realizó correctamente
+    if (insertedId != null) {
+      final noteFromDB =
+          await db?.query('notes', where: 'id = ?', whereArgs: [insertedId]);
+      if (noteFromDB != null && noteFromDB.isNotEmpty) {
+        final encryptedContent = noteFromDB.first['content'] as String?;
+        final ivString = noteFromDB.first['iv'] as String?;
+        final decryptedText = decryptText(encryptedContent!, ivString!);
+        if (decryptedText == textToEncrypt) {
+          print('Encriptación exitosa: $textToEncrypt');
+        } else {
+          print('Error en la encriptación: $textToEncrypt');
+        }
+      }
+    }
+
+    return insertedId;
   }
 
-
-
-  /*
-  Future<List<Notea>?> getAllNotesWithCategoryColors() async {
-    final db = await _instance.db;
-    final result = await db?.rawQuery('''
-    SELECT notes.*, categories.color as category_color
-    FROM notes
-    LEFT JOIN categories ON notes.category_id = categories.id
-  ''');
-
-    return result?.map((e) {
-      final notea = Notea.fromMap(e);
-      notea.color = e['category_color'] as int;
-      return notea;
-    }).toList();
-  }*/
-
-  /*
-  Future<List<Notea>> getAllNotes() async {
-    final db = await _instance.db;
-    final result = await db?.rawQuery('''
-    SELECT notes.*, categories.color AS category_color
-    FROM notes
-    LEFT JOIN categories ON notes.category_id = categories.id
-  ''');
-
-    if (result != null) {
-      return result.map((row) {
-        return Notea(
-          id: row['id'] as int,
-          title: row['title'] as String,
-          content: row['content'] as String,
-          categoryId: row['category_id'] as int,
-          categoryColor: row['category_color'] as int,
-        );
-      }).toList();
-    } else {
-      return [];
-    }
-  }*/
-
-
-  // Actualiza una nota en la base de datos
+  // Función para actualizar una nota en la base de datos.
   Future<int?> update(Notea note) async {
     final db = await _instance.db;
     final id = note.id;
@@ -209,13 +195,13 @@ class DatabaseHelper {
         ?.update('notes', note.toMap(), where: 'id = ?', whereArgs: [id]);
   }
 
-  // Elimina una nota de la base de datos
+  // Función para eliminar una nota de la base de datos.
   Future<int?> delete(int id) async {
     final db = await _instance.db;
     return await db?.delete('notes', where: 'id = ?', whereArgs: [id]);
   }
 }
-// Clase que representa una categoría
+
 class Category {
   int? id;
   String name;
@@ -226,7 +212,7 @@ class Category {
     required this.name,
     required this.color,
   });
-  // Método para convertir un objeto Category en un mapa (para la inserción en la base de datos)
+
   Map<String, dynamic> toMap() {
     return {
       'id': id,
@@ -234,11 +220,10 @@ class Category {
       'color': color,
     };
   }
-  // Constructor factory para crear un objeto Category desde un mapa (resultados de la consulta)
-  factory Category.fromMap(Map<String, dynamic> json) => Category(
-    id: json['id'],
-    name: json['name'],
-    color: json['color'],
-  );
-}
 
+  factory Category.fromMap(Map<String, dynamic> json) => Category(
+        id: json['id'],
+        name: json['name'],
+        color: json['color'],
+      );
+}
